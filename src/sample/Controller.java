1a88b3fad9862e13ff47a315038ac7e69b2d4981
Sanjeev Kumar <ksanjeev284@gmail.com>
2021-03-25 19:50:54 +0530

    Added new functions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,33 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
+import java.net.URL;
 import java.sql.*;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
+
     Connection conn;
     Statement stmt = null;
     ResultSet rs = null;
 
-
+    @FXML
+    Label message;
+    @FXML
+    ListView listViewID;
+    @FXML
+    ListView listViewName;
+    @FXML
+    ListView listViewNumber;
+    @FXML
+    ListView listViewPolicyType;
+    @FXML
+    ListView listViewAddress;
+    @FXML
+    ListView listViewPayment;
     @FXML
     TextField name;
     @FXML
@@ -23,32 +38,59 @@ public class Controller {
     TextField payment;
     @FXML
     ComboBox policySelector;
-    @FXML
-    TableColumn policyNumber;
-    @FXML
-    TableColumn customerName;
-    @FXML
-    TableColumn customerMobile;
-    @FXML
-    TableColumn customerpolicyType;
-    @FXML
-    TableColumn customerAddress;
-    @FXML
-    TableColumn customerPayment;
 
 
 String insertstmnt= "\"INSERT INTO RECORDS(name,mobile,policy,address,policy_payment) VALUES()\"";
-    public void clicked() throws SQLException {
+    public void clicked() {
+        try {
+            stmt.execute("INSERT INTO RECORDS(name,mobile,policy,address,policy_payment) VALUES('"+ name.getText()+ "',"+mobile.getText()+",'"+policySelector.getValue()+"','"+address.getText()+"',"+payment.getText()+")");
+        } catch (SQLException throwables) {
+            if (message.getText()==""){
+                 message.setText("Error: Info Missing");
+            }     else{
+                   message.setText("");
+            }
+
+        }
+
+    }
+
+    public void show() throws SQLException {
+        rs= stmt.executeQuery("SELECT * FROM RECORDS");
+            
+            listViewID.getItems().clear();
+            listViewName.getItems().clear();
+            listViewNumber.getItems().clear();
+            listViewPolicyType.getItems().clear();
+            listViewAddress.getItems().clear();
+            listViewPayment.getItems().clear();
+
+        while(rs.next()){
+            listViewID.getItems().add(rs.getString("policy_no"));
+            listViewName.getItems().add(rs.getString("name"));
+            listViewNumber.getItems().add(rs.getString("mobile"));
+            listViewPolicyType.getItems().add(rs.getString("policy"));
+            listViewAddress.getItems().add(rs.getString("address"));
+            listViewPayment.getItems().add(rs.getString("policy_payment"));
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         try {
             conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/insurance","root","");
             stmt = conn.createStatement();
-            stmt.execute("INSERT INTO RECORDS(name,mobile,policy,address,policy_payment) VALUES('"+ name.getText()+ "',"+mobile.getText()+",'"+policySelector.getValue()+"','"+address.getText()+"',"+payment.getText()+")");
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
-        System.out.println(conn.getSchema());
+        try {
+            show();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
 
     }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,9 +12,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Insurance Management");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setScene(new Scene(root, 1600, 800));
         primaryStage.show();
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
+
 
     }
 
